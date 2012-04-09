dff051067b61d38fce24a172d8bc9614e3f9995c
marcus <mail@marcusschiesser.de>
2012-04-09 18:24:16 +0200

    Anzeige der persistierten Spenden in der Spendeliste

--- my-aktion/src/main/java/de/dpunkt/myaktion/controller/SpendeListController.java
+++ my-aktion/src/main/java/de/dpunkt/myaktion/controller/SpendeListController.java
@@ -1,15 +1,32 @@
 package de.dpunkt.myaktion.controller;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import de.dpunkt.myaktion.model.Aktion;
+import de.dpunkt.myaktion.model.Spende;
 import de.dpunkt.myaktion.model.Spende.Status;
+import de.dpunkt.myaktion.services.ISpendeService;
 
 @RequestScoped
 @Named
 public class SpendeListController {
 	private Aktion aktion;
+	private List<Spende> spenden;
+	
+	@Inject
+	private ISpendeService spendeService;
+
+	public List<Spende> getSpenden() {
+		return spenden;
+	}
+
+	public void setSpenden(List<Spende> spenden) {
+		this.spenden = spenden;
+	}
 
 	public Aktion getAktion() {
 		return aktion;
@@ -17,6 +34,8 @@ public class SpendeListController {
 
 	public void setAktion(Aktion aktion) {
 		this.aktion = aktion;
+		spenden = spendeService.getSpendeList(aktion.getId());
+		System.err.println();
 	}
 
 	public String doOk() {
@@ -24,6 +43,8 @@ public class SpendeListController {
 	}
 	
 	public String convertStatus(Status status) {
+		if(status==null)
+			return "";
 		switch(status) {
 		case UEBERWIESEN:
 			return "überwiesen";
